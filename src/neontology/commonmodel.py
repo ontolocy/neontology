a2ca4e3986d30790c9ff09e2b44268212f56c10b
Ontolocy <dev@ontolocy.com>
2022-10-28 18:54:44 +0100

    v0.1.2 - adding support for neo4j Dates and Times

--- src/neontology/commonmodel.py
+++ src/neontology/commonmodel.py
@@ -3,6 +3,8 @@ from datetime import date, datetime, time, timedelta
 from typing import Any, ClassVar, Dict, List, Optional, Set
 
 from neo4j.time import DateTime as Neo4jDateTime
+from neo4j.time import Date as Neo4jDate
+from neo4j.time import Time as Neo4jTime
 from pydantic import BaseModel, Field, PrivateAttr, root_validator, validator
 
 
@@ -182,7 +184,8 @@ class CommonModel(BaseModel, ABC):
         """
 
         for key in values:
-            if isinstance(values[key], Neo4jDateTime):
+            if isinstance(values[key], (Neo4jDateTime, Neo4jDate, Neo4jTime)):
                 values[key] = values[key].to_native()
+
 
         return values
